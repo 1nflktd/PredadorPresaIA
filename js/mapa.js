f96601bcb6bf32aafc42aa7bad3d1085d3ac03d7
Henrique <henscalon@hotmail.com>
2017-10-21 22:44:18 -0200

    ajuste alert tela

--- js/mapa.js
+++ js/mapa.js
@@ -1,61 +1,63 @@
 function setMapa(ambiente) {
 	var ambiente = JSON.parse(ambiente);
 
-	if (ambiente.LimiteIteracoes) {
-		document.getElementById("mapa").innerHTML = "<p>Limite de 5000 iterações atingido.</p>";
-	} else if (ambiente.PresasTotais <= 0) {
-		document.getElementById("mapa").innerHTML = "<p>Todas as presas foram capturadas.</p>";
-	} else {
-		var CAgentes = {
-			PREDADOR: 1,
-			PRESA: 2,
-			VAZIO: 3,
-			MARCA1: 4,
-			MARCA2: 5,
-			MARCA3: 6,
-			PRESA_FUGINDO: 7
-		};
+	var CAgentes = {
+		PREDADOR: 1,
+		PRESA: 2,
+		VAZIO: 3,
+		MARCA1: 4,
+		MARCA2: 5,
+		MARCA3: 6,
+		PRESA_FUGINDO: 7
+	};
 
-		var mapa = ambiente.Mapa;
+	var mapa = ambiente.Mapa;
 
-	    var tamanhoMapa = ambiente.TamanhoMapa;
-		var table = "<table class=\"table table-bordered table-responsive\">";
-		table += "<tbody>";
-		for (i = 0; i < tamanhoMapa; i++) {
-			table += "<tr>";
-			for (j = 0; j < tamanhoMapa; j++) {
-				var imgName = "";
-				var marca = "";
-				switch(mapa[i][j]) {
-					case CAgentes.PREDADOR:
-						imgName = "predator.jpg";
-						break;
-					case CAgentes.PRESA:
-						imgName = "homer.jpg";
-						break;
-					case CAgentes.PRESA_FUGINDO:
-						imgName = "homer_alt.jpg";
-						break;
-					case CAgentes.MARCA1:
-						marca = "blue"
-						break;
-					case CAgentes.MARCA2:
-						marca = "yellow"
-						break;
-					case CAgentes.MARCA3:
-						marca = "red"
-						break;
-				}
-
-				var img = imgName != "" ? "<img src=\"../images/" + imgName + "\">" : "";
-				var style = marca != "" ? "style='background-color:" + marca + "'" : "";
-				table += "<td " + style + ">" + img + "</td>";
+    var tamanhoMapa = ambiente.TamanhoMapa;
+	var table = "<table class=\"table table-bordered table-responsive\">";
+	table += "<tbody>";
+	for (i = 0; i < tamanhoMapa; i++) {
+		table += "<tr>";
+		for (j = 0; j < tamanhoMapa; j++) {
+			var imgName = "";
+			var marca = "";
+			switch(mapa[i][j]) {
+				case CAgentes.PREDADOR:
+					imgName = "predator.jpg";
+					break;
+				case CAgentes.PRESA:
+					imgName = "homer.jpg";
+					break;
+				case CAgentes.PRESA_FUGINDO:
+					imgName = "homer_alt.jpg";
+					break;
+				case CAgentes.MARCA1:
+					marca = "blue"
+					break;
+				case CAgentes.MARCA2:
+					marca = "yellow"
+					break;
+				case CAgentes.MARCA3:
+					marca = "red"
+					break;
 			}
-			table += "</tr>";
-		}
-		table += "</tbody>";
 
-		document.getElementById("mapa").innerHTML = table;
+			var img = imgName != "" ? "<img src=\"../images/" + imgName + "\">" : "";
+			var style = marca != "" ? "style='background-color:" + marca + "'" : "";
+			table += "<td " + style + ">" + img + "</td>";
+		}
+		table += "</tr>";
+	}
+	table += "</tbody>";
+
+	document.getElementById("mapa").innerHTML = table;
+
+	if (ambiente.LimiteIteracoes) {
+		$(".msgs").hide();
+		$("#limite-iteracoes").show();
+	} else if (ambiente.PresasTotais <= 0) {
+		$(".msgs").hide();
+		$("#presas-capturadas").show();
 	}
 }
 
@@ -67,7 +69,8 @@ function iniciarEventSource() {
 	};
 
 	source.onerror = function(e){
-		document.getElementById("mapa").innerHTML = "<p>Servidor morreu!</p>";
+		$(".msgs").hide();
+		$("#servidor-morto").show();
 	};
 }
 
